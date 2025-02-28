6f9ab59b013012bc9cc038fc9b8d248d916797b0
Patrick Jones <patrick_jones@epam.com>
2025-02-28 18:14:07 +0100

    RadarPacketReader class completed

--- Radar/RadarPacketPcapReader.py
+++ Radar/RadarPacketPcapReader.py
@@ -1,12 +1,11 @@
 from Radar.RadarPacketReader import RadarPacketReader, tqdm
-from scapy.all import rdpcap, UDP, IP
 import numpy as np
+from pyshark import FileCapture
 
-
-class RadarPacketPcapngReader(RadarPacketReader):
+class RadarPacketPcapReader(RadarPacketReader):
     def __init__(self, filename, rdc_file="rdc_file.npy"):
         super().__init__(filename, rdc_file)
-        self.pcap = rdpcap(filename)
+        self.pcap = FileCapture(filename)
         self.rdc_packets = []
         self.properties_packets = []
         print(f"Reading {filename}")
@@ -14,32 +13,23 @@ class RadarPacketPcapngReader(RadarPacketReader):
         self.extract_radar_cube_data()
         self.fill_properties()
         
+    def _udp_payload(self, pkt):
+        return bytearray.fromhex(pkt.data.data)
+        
 
     def _read_packets(self):
         def filter_packets(pkt):
-            # print(pkt)
-            # print(pkt.summary())
-            # print(pkt.haslayer("UDP"))
-            # print(UDP in pkt)
-            # # print(pkt[UDP])
-            # # print(pkt[UDP].dport)
-            # print(IP in pkt)
-            # # print(pkt[IP])
-            # # print(pkt[IP].src)
-            # # print(pkt[IP].dst)
-            # # exit()
-            if UDP in pkt and pkt[UDP].dport == self.RADAR_CUBE_UDP_PORT and pkt[IP].src == self.IP_SOURCE and pkt[IP].dst == self.IP_DEST :
-                self.rdc_packets.append(pkt)
-            elif UDP in pkt and pkt[UDP].dport == self.BIN_PROPERTIES_UDP_PORT and pkt[IP].src == self.IP_SOURCE and pkt[IP].dst == self.IP_DEST:
+            print(pkt)
+            exit()
+            if "udp" in pkt and int(pkt["udp"].dstport) == self.BIN_PROPERTIES_UDP_PORT and pkt["ip"].src == self.IP_SOURCE and pkt["ip"].dst == self.IP_DEST:
                 self.properties_packets.append(pkt)
+            elif "udp" in pkt and int(pkt["udp"].dstport) == self.RADAR_CUBE_UDP_PORT and pkt["ip"].src == self.IP_SOURCE and pkt["ip"].dst == self.IP_DEST:
+                self.rdc_packets.append(pkt)
+
         self.progress_bar(self.pcap, filter_packets, "Filtering packets")
-        # for pkt in self.pcap:
-        #     if UDP in pkt and pkt[UDP].dport == self.RADAR_CUBE_UDP_PORT and pkt[IP].src == self.IP_SOURCE and pkt[IP].dst == self.IP_DEST :
-        #         self.rdc_packets.append(pkt)
-        #     elif UDP in pkt and pkt[UDP].dport == self.BIN_PROPERTIES_UDP_PORT and pkt[IP].src == self.IP_SOURCE and pkt[IP].dst == self.IP_DEST:
-        #         self.properties_packets.append(pkt)
+    
         def get_sort_key(pkt):
-            payload = pkt[UDP].payload.load
+            payload = self._udp_payload(pkt)
             return (
                 int.from_bytes(payload[14:18], byteorder="big"),
                 int.from_bytes(payload[10:12], byteorder="big"),
@@ -47,10 +37,10 @@ class RadarPacketPcapngReader(RadarPacketReader):
         self.rdc_packets.sort(key=get_sort_key)
         self.properties_packets.sort(key=get_sort_key)
         first_valid_packet = 0
-        while self.rdc_packets[first_valid_packet][UDP].payload.load[18] != 0x01:
+        while self._udp_payload(self.rdc_packets[0])[18] != 0x01:
             first_valid_packet += 1
         self.rdc_packets = self.rdc_packets[first_valid_packet:]
-        first_payload = bytes(self.rdc_packets[0][UDP].payload)
+        first_payload = bytes(self._udp_payload(self.rdc_packets[0]))
         self.fields = self.extract_header(first_payload)
         
         # Define radar cube dimensions
@@ -70,18 +60,13 @@ class RadarPacketPcapngReader(RadarPacketReader):
         nb_packets = len(self.rdc_packets)
         with tqdm(total=nb_packets, desc="Extracting RDC") as pbar:
             while i<nb_packets:
-                while i<nb_packets and self.rdc_packets[i][UDP].payload.load[18] != 0x01:    # Skip invalid 1st packet of frame
-                    i+=1; pbar.update(1)
-                
-                radar_cube_data = bytearray(self.rdc_packets[i][UDP].payload.load[22+64:])
-                current_frame = self.rdc_packets[i][UDP].payload.load[14:18]
-                timestamp = np.frombuffer(self.rdc_packets[i][UDP].payload.load[30:38], dtype=self.dt_uint64)[0]
+                radar_cube_data = bytearray(self._udp_payload(self.rdc_packets[i])[22+64:])
+                current_frame = self._udp_payload(self.rdc_packets[i])[14:18]
+                timestamp = np.frombuffer(self._udp_payload(self.rdc_packets[i])[30:38], dtype=self.dt_uint64)[0]
                 i+=1; pbar.update(1)
-                nb_packets_found = 1
-                while i<nb_packets and self.rdc_packets[i][UDP].payload.load[18] != 0x01 and self.rdc_packets[i][UDP].payload.load[14:18] == current_frame:
-                    radar_cube_data.extend(self.rdc_packets[i][UDP].payload.load[22:])
-                    # print(str(self.rdc_packets[i][UDP].payload.load[18]), end="")
-                    i+=1; pbar.update(1); nb_packets_found+=1
+                while i<nb_packets and self._udp_payload(self.rdc_packets[i])[18] != 0x01 and self._udp_payload(self.rdc_packets[i])[14:18] == current_frame:
+                    radar_cube_data.extend(self._udp_payload(self.rdc_packets[i])[22:])
+                    i+=1; pbar.update(1)
                 radar_cube_data = self.process_radar_cube_data(radar_cube_data)
                 if radar_cube_data is not None:
                     self.timestamps.append(timestamp)
@@ -96,7 +81,7 @@ class RadarPacketPcapngReader(RadarPacketReader):
         self.all_properties = []
         all_frames_counter = []
         for i in range(len(self.properties_packets)):
-            property_payload = bytes(self.properties_packets[i][UDP].payload)
+            property_payload = bytes(self._udp_payload(self.properties_packets[i]))
             frame_counter, properties = self.extract_properties(property_payload)
             all_frames_counter.append(frame_counter)
             self.all_properties.append(properties)
@@ -116,7 +101,7 @@ class RadarPacketPcapngReader(RadarPacketReader):
         first_properties = self.all_properties[0]
         print(f"First property frame :{first_property_frame}")
         
-        first_radar_cube_frame = np.frombuffer(self.rdc_packets[0][UDP].payload.load[14:18], dtype=self.dt_uint32)[0]
+        first_radar_cube_frame = np.frombuffer(self._udp_payload(self.rdc_packets[0])[14:18], dtype=self.dt_uint32)[0]
         frame_diff = first_radar_cube_frame - first_property_frame
         print(f"First Radar Cube Frame: {first_radar_cube_frame}, First Property Frame: {first_property_frame}, Frame Diff: {frame_diff}")
         if first_property_frame < first_radar_cube_frame:
@@ -125,12 +110,14 @@ class RadarPacketPcapngReader(RadarPacketReader):
             for i in range(frame_diff):
                 self.all_properties.insert(0, first_properties)
                 print(f"Missing property frame: {first_radar_cube_frame-i}")
-                
-        if (len(self.radar_cube_datas) != len(self.all_properties)):
-            print(f"Radar Cube Data: {len(self.radar_cube_datas)}, Properties: {len(self.all_properties)}")
-            for i in range(len(self.radar_cube_datas)-len(self.all_properties)):
+        len_diff = len(self.radar_cube_datas) - len(self.all_properties)        
+        print(f"Radar Cube Data: {len(self.radar_cube_datas)}, Properties: {len(self.all_properties)}")
+        if (len_diff > 0):
+            for i in range(len_diff):
                 self.all_properties.insert(-1, self.all_properties[-1])
-            print(f"Filled Properties: {len(self.all_properties)}")
+        else:
+            self.all_properties = self.all_properties[:len(self.radar_cube_datas)]
+        print(f"Filled Properties: {len(self.all_properties)}")
         
 
     def __iter__(self):
--- Radar/RadarPacketReader.py
+++ Radar/RadarPacketReader.py
@@ -1,13 +1,15 @@
 import numpy as np
+from tqdm import tqdm
 
 class RadarPacketReader:
-    def __init__(self, filename):
+    def __init__(self, filename, rdc_file="rdc_file.npy"):
         self.filename = filename
+        self.rdc_file = rdc_file
         self.fields = None
         self.timestamps = None
         self.nb_frames = None
         self.all_properties = None
-        self.radar_cube_data = None
+        self.radar_cube_datas = None
         
         # Define parameters
         self.IP_SOURCE = "192.168.11.11"
@@ -16,32 +18,91 @@ class RadarPacketReader:
         self.BIN_PROPERTIES_UDP_PORT = 50063  # Bin Properties port
         self.header_size = 22 + 64  # Header size from document
         
-        self.read_file()
-       
-    def read_file(self):
-        pass
+        # Data fields greater than 1 Byte are transmitted in Big Endian.
+        self.dt_uint64 = np.dtype(np.uint64).newbyteorder('>')
+        self.dt_uint32 = np.dtype(np.uint32).newbyteorder('>')
+        self.dt_uint16 = np.dtype(np.uint16).newbyteorder('>')
+        self.dt_int16 = np.dtype(np.int16).newbyteorder('>')
+        self.dt_float32 = np.dtype(np.float32).newbyteorder('>')
+        
+        # Radar cube dimensions
+        self.N_RANGE_GATES       = None
+        self.N_DOPPLER_BINS      = None
+        self.N_RX_CHANNELS       = None
+        self.N_CHIRP_TYPES       = None
+        self.FIRST_RANGE_GATE    = None
+        
     
-    def extract_header(data):
+    def extract_header(self, data):
         data_ = data[22:] # Skip the first 22 bytes
         header_size = 64
         header = data_[24:header_size] # Extract the header
         offsets = header[:24]
-        imag_offset, real_offset, range_gate_offset, doppler_bin_offset, rx_channel_offset, chirp_type_offset = np.frombuffer(offsets, dtype=dt_uint32)
+        imag_offset, real_offset, range_gate_offset, doppler_bin_offset, rx_channel_offset, chirp_type_offset = np.frombuffer(offsets, dtype=self.dt_uint32)
         indexes = header[24:30]
-        indexes = np.frombuffer(indexes, dtype=dt_uint16)
+        indexes = np.frombuffer(indexes, dtype=self.dt_uint16)
         range_gates, first_range_gate, doppler_bins = indexes
         rx_channels, chirp_types, element_size, element_type = header[30:34]
         
         # Return np array to be saved
         return np.array([imag_offset, real_offset, range_gate_offset, doppler_bin_offset, rx_channel_offset, chirp_type_offset, range_gates, first_range_gate, doppler_bins, rx_channels, chirp_types, element_size, element_type])
     
-    def extract_radar_cube_data(data):
-        pass
+    def process_radar_cube_data(self, radar_data):
+        # Check if the data size is correct
+        expected_size = self.N_RANGE_GATES * self.N_DOPPLER_BINS * self.N_RX_CHANNELS * self.N_CHIRP_TYPES * 2 * 2
+        if len(radar_data) != expected_size: #Skip incomplete frames
+            # print(f"Expected size: {expected_size}, Received size: {len(radar_data)}")
+            print("!", end="")
+            return None
+        print(".", end="")
+        # Convert to NumPy array and reshape
+        radar_cube = np.frombuffer(radar_data, dtype=self.dt_int16)
+        radar_cube = radar_cube.reshape((self.N_CHIRP_TYPES, self.N_RANGE_GATES, self.N_RX_CHANNELS, self.N_DOPPLER_BINS, 2))
+        # Transpose to (range gate, doppler, rx, chirp)
+        radar_cube = np.transpose(radar_cube, (1, 3, 2, 0, 4))
+
+        # Extract real and imaginary parts
+        real_part = radar_cube[..., 0]
+        imag_part = radar_cube[..., 1]
+
+        # Convert to complex values
+        complex_data = real_part + 1j * imag_part
+
+        range_doppler_matrix = np.fft.fftshift(complex_data, axes=1)
+
+        # Return np array to be saved
+        return range_doppler_matrix
     
-    def extract_properties(data):
-        frame_counter  = np.frombuffer(data[14:18], dtype=dt_uint32)[0]
+    def extract_properties(self, data):
+        frame_counter  = np.frombuffer(data[14:18], dtype=self.dt_uint32)[0]
         properties = data[22+24:]
-        properties = np.frombuffer(properties, dtype=dt_float32)
+        properties = np.frombuffer(properties, dtype=self.dt_float32)
 
         # Return np array to be saved
         return (frame_counter, properties)
+    
+    def save(self):        
+        with open(self.rdc_file, "wb") as f:
+            np.save(f, self.fields)
+            np.save(f, self.timestamps)
+            np.save(f, self.nb_frames)
+            for i in range(self.nb_frames):
+                np.save(f, self.all_properties[i])
+            for i in range(self.nb_frames):
+                np.save(f, self.radar_cube_datas[i])
+                
+    def load(self):
+        with open(self.rdc_file, 'rb') as f:
+            self.fields = np.load(f, allow_pickle=True)
+            self.timestamps = np.load(f, allow_pickle=True)
+            self.nb_frames = np.load(f, allow_pickle=True)
+            self.all_properties = np.array([np.load(f, allow_pickle=True)])
+            for _ in range(self.nb_frames-1):
+                self.all_properties = np.append(self.all_properties, [np.load(f, allow_pickle=True)], axis=0)
+            self.radar_cube_datas = np.array([np.load(f, allow_pickle=True)])
+            for _ in range(self.nb_frames-1):
+                self.radar_cube_datas = np.append(self.radar_cube_datas, [np.load(f, allow_pickle=True)], axis=0)
+                
+    def progress_bar(self, iterable, func, description):
+        for i in tqdm(iterable, desc=description):
+            func(i)
