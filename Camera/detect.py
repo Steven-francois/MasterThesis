57befd6905d32baa648c09b5ca411cd70db93344
Steven François <steven.francois@student.uclouvain.be>
2025-05-28 17:54:26 +0200

    Finished detection on camera save in npy and json for cong and classes

--- Camera/detect.py
+++ Camera/detect.py
@@ -1,27 +1,34 @@
 from ultralytics import YOLO
+import os
+import numpy as np
+import json
+from tqdm import trange
 
 # Load a model
-model = YOLO("yolo11n.pt")
+model = YOLO("yolov9e.pt")
 
 # Train the model
 # results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
 
-# Predict with the model
-results = model.predict(source="Fusion/data/1/camera/2025-04-17_15-40-41-456810.png", save=True, show=True)
-print(results)
-# results = model.predict(source="Fusion/captures30/camera_rgba/2025-03-12_12-37-38-739616.png", save=True, conf=0.5, show=True)
-for result in results:
-    xywh = result.boxes.xywh  # center-x, center-y, width, height
-    xywhn = result.boxes.xywhn  # normalized
-    xyxy = result.boxes.xyxy  # top-left-x, top-left-y, bottom-right-x, bottom-right-y
-    xyxyn = result.boxes.xyxyn  # normalized
-    names = [result.names[cls.item()] for cls in result.boxes.cls.int()]  # class name of each box
-    confs = result.boxes.conf  # confidence score of each box
-    
-    print("xywh:", xywh)
-    print("xywhn:", xywhn)
-    print("xyxy:", xyxy)
-    print("xyxyn:", xyxyn)
-    print("names:", names)
-    print("confs:", confs)
-    print("--------------------------------------------------")
+nb = "1_0"
+data_folder = f"Data/{nb}/"
+image_folder = os.path.join(data_folder, "camera")
+image_filenames = sorted(os.listdir(image_folder))
+images = [os.path.join(image_folder, filename) for filename in image_filenames]
+targets_folder = os.path.join(data_folder, "camera", "targets")
+os.makedirs(targets_folder, exist_ok=True)
+
+
+data = []
+with open(os.path.join(targets_folder, "targets.npy"), "wb") as f:
+    np.save(f, len(images))  # Save the number of results
+    for i in trange(len(images), desc="Processing images"):
+        result = model.predict(images[i], save=True, show=False, conf=0.1, project=image_folder, name='targets', exist_ok=True, classes=[0,1,2,3,5,7], verbose=False)[0]
+        data.append({
+            "conf": result.boxes.conf.cpu().tolist(),
+            "cls": result.boxes.cls.cpu().tolist()
+        })
+        np.save(f, result.boxes.xywh.cpu().numpy())
+with open(os.path.join(targets_folder, "targets.json"), "w") as f:
+    json.dump(data, f)
+
